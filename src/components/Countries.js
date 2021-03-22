d150ed19678edcfb7e47cdef38386c65ca8594aa
marquillier <nicolas.marquillier@elosi.com>
2021-03-22 23:05:43 +0100

    mise en place du useEffect pour recuperation des donnees

---
+++ src/components/Countries.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+
+const Countries = () => {
+    // Ceci est un hook
+    // data = c'est notre variable, là ou on va mettre nos données
+    // setData = c'est lélément par lequel on passera pour actualiser les données
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag")
+            .then((res) => setData(res.data));
+
+        console.log(data);
+
+    // les "[]" permettent de ne jouer qu'une seul fois le useEffect
+    }, []);
+    return (
+        <div></div>
+    );
+
+};
+
+export default Countries;
