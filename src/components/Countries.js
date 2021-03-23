097d8f2b8629d52423dc37587e80dd00d7876dbc
marquillier <nicolas.marquillier@elosi.com>
2021-03-23 14:33:04 +0100

    mise en place des boutons radio - WIP

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -10,14 +10,16 @@ const Countries = () => {
     const [sortedData, setSortedData] = useState([]);
     const [playOnce, setPlayOnce] = useState(true);
     const [rangeValue, setRangeValue] = useState(40);
+    const [selectedRadio, setSelectedRadio] = useState('');
+    const radio = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
 
     useEffect(() => {
         if (playOnce) {
             axios.get("https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag")
-                .then((res) => { 
-                    setData(res.data); 
-                    setPlayOnce(false); 
+                .then((res) => {
+                    setData(res.data);
+                    setPlayOnce(false);
                 });
         }
 
@@ -38,8 +40,19 @@ const Countries = () => {
         <div className="countries">
             <div className="sort-container">
                 <input type="range" min="1" max="250" value={rangeValue}
-                onChange={(e) => setRangeValue(e.target.value)}/>
-            </div> 
+                    onChange={(e) => setRangeValue(e.target.value)} />
+                <ul>
+                    {radio.map((radio) => {
+                        return (
+                            <li key={radio}>
+                                <input type="radio" value={radio} id={radio} 
+                                checked={radio === selectedRadio} onChange={(e) => setSelectedRadio(e.target.value)}/>
+                                <label htmlFor={radio}>{radio}</label>
+                            </li>
+                            )
+                        })}
+                </ul>
+            </div>
             <ul className="countries-list">
                 {sortedData.map((country) => (
                     <Card country={country} key={country.name} />
